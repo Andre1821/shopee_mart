92e6293a370d415b46058908f87292c06906890c
fabianandre <fabianandrefajar@gmail.com>
2023-12-13 16:46:31 +0700

    Auth 1

--- src/main/java/com/enigma/shopeymart/service/impl/UserServiceImpl.java
+++ src/main/java/com/enigma/shopeymart/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.enigma.shopeymart.entity.UserCredential;
 import com.enigma.shopeymart.repository.UserCredentialRepository;
 import com.enigma.shopeymart.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,18 @@ public class UserServiceImpl implements UserService {
     @Override
     public AppUser loadUserByUserId(String id) { //method ini untuk memverifikasi JWTnya
         UserCredential userCredential = userCredentialRepository.findById(id).orElseThrow(()-> new UsernameNotFoundException("Invalid credential"));
+        return AppUser.builder()
+                .id(userCredential.getId())
+                .username(userCredential.getUsername())
+                .password(userCredential.getPassword())
+                .role(userCredential.getRole().getName())
+                .build();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // method ini untuk cek by usernamenya sebagai authentication untuk login
+        UserCredential userCredential = userCredentialRepository.findByUsername(username).orElseThrow(()-> new UsernameNotFoundException("Invalid credential"));
         return AppUser.builder()
                 .id(userCredential.getId())
                 .username(userCredential.getUsername())
